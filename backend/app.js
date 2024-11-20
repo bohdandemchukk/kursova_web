a1aa41ce6f79bab4396cc2f16dc05b1199e963e6
bohdandemchukk bc2024-1 <bogdanchuk2006@gmail.com>
2024-11-20 18:02:24 +0200

    backend register/login

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
-const app = express()
+const app = express();
+const authRoutes = require('./routes/authRoutes');
 
 require('dotenv').config()
 
@@ -11,6 +12,9 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+
+app.use('/api/auth', authRoutes);
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
